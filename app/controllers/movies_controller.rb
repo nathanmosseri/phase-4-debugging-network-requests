a552b41a1111c94e14790330720c83fdc7d572a7
Nathan <namo678@me.com>
2022-09-21 13:27:28 -0400

    commit tree 46dbf21e8b5e6d5c3cd8453565f59a3f6aefeed7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,34 @@
 class MoviesController < ApplicationController
+
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_nonprocessable_entity
   
   def index
     movies = Movie.all
     render json: movies
+  end
+
+  def create 
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created 
+  end
+
+  private 
+
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
+  def find_movie
+    Movie.find(params[:id])
+  end
+
+  def render_not_found
+    render json: {error: 'Movie not found'}, status: :not_found
+  end
+
+  def render_nonprocessable_entity(invalid)
+    render json: {errors: invalid.record.errors}, status: :nonprocessable_entity
   end
 
 end
